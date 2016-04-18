253af48a58ee3255e461395bc17bac73ccb259ec
Sripadalns <sripadalns@gmail.com>
2016-04-18 18:19:14 +0530

    Added files via upload

---
+++ Pwm.h
@@ -0,0 +1,25 @@
+/*
+ * Pwm.h
+ *
+ * Created: 18-04-2016 14:20:53
+ *  Author: sripals
+ */ 
+
+
+#ifndef PWM_H_
+#define PWM_H_
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+/* Initialize PWM module */
+void InitPWM(void);
+
+/* Set Duty */
+void SetPWMDuty(uint8_t duty);
+
+
+
+
+#endif /* PWM_H_ */
