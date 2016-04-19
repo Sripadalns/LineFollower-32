83cd5ed19e3855b438a1f082616ef33bedbe1bb6
Sripadalns <sripadalns@gmail.com>
2016-04-19 17:08:34 +0530

    Added files via upload

---
+++ Robot.c
@@ -0,0 +1,16 @@
+/*
+ * Robot.c
+ *
+ * Created: 19-04-2016 13:45:42
+ *  Author: sripals
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
